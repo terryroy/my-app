b1ea32ccfaa45cdc14d23349c118a9e42ad6c1e5
Terry Roy <troy@cadgov.com>
2017-02-01 15:11:43 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+
+
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Tour of Heroes';
+  subtitle = 'My Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  //HeroService code
+  //private constructor
+  constructor(private heroService: HeroService) { }
+
+  //Promise call for getHeroes
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes)
+  }
+
+  //Init call to retrieve getHeroes()
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
