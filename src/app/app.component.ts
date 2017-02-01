a8bbd73853f664050f75c2c9177b38e0da51eb28
terryroy <troy@cadgov.com>
2017-02-01 17:06:24 -0500

    02012017

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,10 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
-
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [HeroService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of Heroes';
-  subtitle = 'My Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  //HeroService code
-  //private constructor
-  constructor(private heroService: HeroService) { }
-
-  //Promise call for getHeroes
-  getHeroes(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes)
-  }
-
-  //Init call to retrieve getHeroes()
-  ngOnInit(): void {
-    this.getHeroes();
-  }
-
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
